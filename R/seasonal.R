e5711fff86f6009572d7dc9980c66c976745ceff
Keith Ng <1203875+keithng@users.noreply.github.com>
2024-12-16 15:33:18 +1300

    Update seasonal.R

--- R/seasonal.R
+++ R/seasonal.R
@@ -4,17 +4,28 @@
 #' @name make_seasonal
 #' @param val Value column
 #' @param period Period column
+#' @param imputed_values Return imputed values
 #' @keywords hud clean seasonal seasonality
 #' @importFrom zoo read.zoo as.yearmon
 #' @importFrom seasonal seas
 #' @export
 #' @examples
 #' df %>% mutate(value_sadj = make_seasonal(value, period))
-make_seasonal <- function(val, period) {
-  s_res <-
+make_seasonal <- function(val, period, imputed_values = FALSE) {
+  raw_ts <-
     data.frame(period, val) %>%
-    read.zoo(FUN = as.yearmon) %>%
-    as.ts() %>%
-    seas()
-  s_res$data[,"seasonaladj"]
+    zoo::read.zoo(FUN = zoo::as.yearmon) %>%
+    as.ts()
+
+  # Impute missing data for the seasonal calculation
+  s_res <-
+    raw_ts %>%
+    zoo::na.StructTS() %>%
+    seasonal::seas()
+
+  if (imputed_values) {
+    s_res$data[, "seasonaladj"]
+  } else {
+    s_res$data[!is.na(raw_ts), "seasonaladj"]
+  }
 }
